cbc2256e1650c2ed97b34b64048b47aca0f85dcd
Victor Phos <victophos1@gmail.com>
2026-05-10 01:46:31 +0200

    improv: prevent errors from going to stdoutput file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -10,17 +10,17 @@ fn main() {
 
 
    let config = Config::new(&args).unwrap_or_else(|err| {
-       println!("Problem parsing arguments: {}", err);
+       eprintln!("Problem parsing arguments: {}", err);
        process::exit(1);
    });
 
    
-    println!("{:?}", args);
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
+    // println!("{:?}", args);
+    // println!("Searching for {}", config.query);
+    // println!("In file {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     };
 }
