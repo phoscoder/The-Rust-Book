6eae2c76a63caa08037413ab9d217ac9f994625f
Victor Phos <victophos1@gmail.com>
2026-05-10 01:44:15 +0200

    feat: can do case inensitive search from the CLI

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,11 +1,18 @@
 
 use std::fs;
 use std::error::Error;
+use std::env;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    }else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
@@ -15,6 +22,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -26,8 +34,9 @@ impl Config {
 
         let query = args[1].clone();
         let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config {query, filename})
+        Ok(Config {query, filename, case_sensitive})
     }
 }
 
