d14b37d2400bdfb3e71ba3ce0d69bbaa6044bd40
Victor Phos <victophos1@gmail.com>
2026-05-10 01:22:59 +0200

    feat: add search fn and also tests

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -27,3 +27,29 @@ impl Config {
         Ok(Config {query, filename})
     }
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+        
+        let mut results = Vec::new();
+
+        for line in contents.lines() {
+            if line.contains(query){
+                results.push(line);
+            }
+        }
+        results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+       let query = "duct";
+       let contents = "\
+       Rust:\nsafe, fast, productive.\nPick three.";
+
+         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
