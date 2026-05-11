293e11ab757985835a67b57a7c8b6a0891ecf1ce
Victor Phos <victophos1@gmail.com>
2026-05-11 04:11:01 +0200

    chore: commit all pending changes

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -7,12 +7,12 @@ pub trait Iterator {
 
 
 #[derive(PartialEq, Debug)]
-struct Show {
+struct Shoe {
     size: u32,
     style: String,
 }
 
-fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: uu32) -> Vec<Shoe> {
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
     shoes.into_iter().filter(|s| s.size == shoe_size).collect()
 }
 
@@ -32,5 +32,31 @@ fn main() {
     let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
     println!("v2: {:?}", v2);
 
+    
+
 
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe { size: 10, style: String::from("sneaker") },
+            Shoe { size: 13, style: String::from("sandal") },
+            Shoe { size: 10, style: String::from("boot") },
+        ];
+
+        let in_my_size = shoes_in_my_size(shoes, 10);
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe { size: 10, style: String::from("sneaker") },
+                Shoe { size: 10, style: String::from("boot") },
+            ]
+        );
+    }
+}   
+
---
+++ workspaces/adder/src/main.rs
@@ -0,0 +1,7 @@
+
+use add_one;
+
+fn main() {
+    let num: u64 = 10;
+    println!("Hello, world! {} plus one is {}!", num, add_one::add(num));
+}
