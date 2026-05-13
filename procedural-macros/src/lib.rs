1762df64df8f3bd48da46dbf597d19e75faa2a01
Victor Phos <victophos1@gmail.com>
2026-05-13 22:08:27 +0200

    Add placeholder notes for attribute-like procedural macro implementation

---
+++ procedural-macros/src/lib.rs
@@ -0,0 +1,8 @@
+// use proc_macro;
+
+
+// #[some_attribute]
+// pub fn some_macro(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+//     // do something with the input tokens and produce output tokens
+
+// }
